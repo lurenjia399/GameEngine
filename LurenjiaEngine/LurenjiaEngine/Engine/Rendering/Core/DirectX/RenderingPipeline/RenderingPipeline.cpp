7de6b48e29d108c9fc4805965bafcb73bf60bbae
人甲 路 <2421707150@qq.com>
2023-05-23 23:15:31 +0800

    重新走遍流程，添加注释，体会堆的使用

--- LurenjiaEngine/LurenjiaEngine/Engine/Rendering/Core/DirectX/RenderingPipeline/RenderingPipeline.cpp
+++ LurenjiaEngine/LurenjiaEngine/Engine/Rendering/Core/DirectX/RenderingPipeline/RenderingPipeline.cpp
@@ -39,24 +39,43 @@ void FRenderingPipeline::BuildPipeline()
 	}
 	DirectXPiepelineState.BindRootSignature(DirectXRootSignature.GetRootSignature());
 
-	//创建模型资源（顶点和索引）缓冲区
-	GeometryMap.BuildMeshBuffer();
+	{
+		/*
+		* 这部分资源是模型的顶点、索引数据啥的
+		* 这些数据由cpu计算，然后通过上传堆，把数据放到默认堆里面，给gpu读取
+		*/
+		//创建模型资源（顶点和索引）缓冲区
+		GeometryMap.BuildMeshBuffer();
+	}
 
-	//构建雾的常量缓冲区
-	GeometryMap.BuildFogConstantBufferView();
-	//构建模型的常量缓冲区
-	GeometryMap.BuildMeshConstantBufferView();
-	//构建材质的shader资源缓冲区,收集所有的matrial，并且设置material中的shaderindex，这个不是常量缓冲区，所以没有放在描述堆中
-	GeometryMap.BuildMaterialShaderResourseView();
-	//构建灯光的常量缓冲区
-	GeometryMap.BuildLightConstantBufferView();
-	//构建视口的常量缓冲区
-	GeometryMap.BuildViewportConstantBufferView();
+	{
+		/*
+		* 这部分资源应该是常量，也就是通过cpu来计算数据，然后通过将数据与上传堆映射，进而等同于放到上传堆中
+		* gpu每次渲染的时候读取上传堆中的数据，进而在着色器中计算
+		* 每次渲染tick，数据都会由cpu重新计算更新，所以放在上传堆中
+		*/
+		//构建雾的上传堆（也就是创建上传堆，并将雾的数据和上传堆映射起来，下面这几个同样如此）
+		GeometryMap.BuildFogConstantBufferView();
+		//构建模型的上传堆
+		GeometryMap.BuildMeshConstantBufferView();
+		//构建材质的上传堆,收集所有的matrial，并且设置material中的shaderindex，
+		GeometryMap.BuildMaterialShaderResourseView();
+		//构建灯光的上传堆
+		GeometryMap.BuildLightConstantBufferView();
+		//构建视口的上传堆
+		GeometryMap.BuildViewportConstantBufferView();
+	}
 
-	//创建描述符堆（用于存放常量缓冲区中的资源）, 这里面全是贴图资源
-	GeometryMap.BuildDescriptorHeap();
-	//构建贴图的着色器资源
-	GeometryMap.BuildTextureShaderResource();
+	{
+		/*
+		* 这部分资源（贴图资源）应该是在程序运行当中不会改变，然后就把他们创建成了CreateShaderResourceView
+		* 然后放在一起，组成描述符堆
+		*/
+		//创建描述符堆（用于存放贴图资源）
+		GeometryMap.BuildDescriptorHeap();
+		//构建贴图的着色器资源，将它们放到上边的描述符堆中的描述符中，也就是当作数组元素
+		GeometryMap.BuildTextureShaderResource();
+	}
 
 	//构建pso
 	FRenderLayerManage::GetRenderLayerManage()->BuildPSO();
